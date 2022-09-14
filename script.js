cd840a112c4f046f121ab1c8fbc22fe722469d70
S-Dandara <daandara.s@gmail.com>
2022-09-14 21:53:19 +0100

    Implement a function that plays a single round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
-console.log(`Rock Paper Scissors`)
-
+//Create function getComputerChoice to play against player.
 function getComputerChoice () {
-    let choice = (Math.floor(Math.random()*3))+1
+    let computer = (Math.floor(Math.random()*3))+1
     let computerChoice;
-    if (choice == 1){
+    if (computer == 1){
         computerChoice = `rock`
     }
-    else if (choice == 2){
+    else if (computer == 2){
         computerChoice = `paper`
     }
-    else if (choice == 3){
+    else if (computer == 3){
         computerChoice = `scissor`
     }
     else{
         computerChoice = `Error!`
     }
-    
+     
     return computerChoice
 }
-console.log(getComputerChoice());
+
+//
+let playerChoice = window.prompt(`Rock, paper or scissor? Make your choice:`);
+playerChoice = playerChoice.toLowerCase();
+
+//Create function that plays one round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection){
+    let result = ``;
+    if (playerSelection === `rock` && computerSelection === `scissor` || playerSelection === `scissor` && computerSelection === `paper` || playerSelection === `paper` && computerSelection === `rock`){
+        result = `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+    else if (computerSelection === `rock` && playerSelection === `scissor` || computerSelection === `scissor` && playerSelection === `paper` || computerSelection === `paper` && playerSelection === `rock`){
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+    else if (playerSelection === computerSelection){
+        result = `It's a tie!`
+    }
+
+    return result;
+}
+
+//Storing the play choices in variables that will be used as parameters for the playRound function
+let playerSelection = playerChoice;
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
