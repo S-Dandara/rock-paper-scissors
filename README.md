e8691b8b1640caf075d86005030e1ea49de8b279
S-Dandara <daandara.s@gmail.com>
2022-09-18 00:41:52 +0100

    Add game function and README update

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# rock-paper-scissors
+# Rock, paper, scissors project
+
+```
+### **Project view:**
+
+https://s-dandara.github.io/rock-paper-scissors/
+
+```
+
+In this project I was able to practice with functional programing to make a simple implementation of grade-school classic “rock paper scissors” to be played from the browser console against the machine.
+<br>
+<br>
+1. Write a function that will randomly return either `Rock`, `Paper` or `Scissors` as computer play.<br>
+2. Write a function that plays a single round of `Rock, Paper, Scissors`, that take two parameters - the `playerSelection` and `computerSelection` - and return the result of the round.
+    >The function's playerSElection parameter should be case-insensitive (so users can input paper, PAPER, PapEr or any other variation).
+3. Write a function that will play 5 rounds of the game, keeps score and return the results at the end.
+<br><br>
+
+We have four functions:<br>
+    - `getComputerChoice`: randomly return computer's choice<br>
+    - `getPlayerChoice`: opens a prompt to get the player choice<br>
+    - `playRound`: call's the two previous functions and returns the round result<br>
+    - `game`: call's the `playRound` function 5 times and returns the game result<br>
--- script.js
+++ script.js
@@ -9,37 +9,76 @@ function getComputerChoice () {
         computerChoice = `paper`
     }
     else if (computer == 3){
-        computerChoice = `scissor`
+        computerChoice = `scissors`
     }
     else{
         computerChoice = `Error!`
+
     }
      
     return computerChoice
 }
 
-//
-let playerChoice = window.prompt(`Rock, paper or scissor? Make your choice:`);
-playerChoice = playerChoice.toLowerCase();
+//Create prompt to receive playerSelection
+function getPlayerChoice(){
+    let playerChoice = window.prompt(`Rock, paper or scissors? Make your choice:`);
+    playerChoice = playerChoice.toLowerCase();
+    return playerChoice;
+}
 
 //Create function that plays one round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection){
     let result = ``;
-    if (playerSelection === `rock` && computerSelection === `scissor` || playerSelection === `scissor` && computerSelection === `paper` || playerSelection === `paper` && computerSelection === `rock`){
-        result = `You Win! ${playerSelection} beats ${computerSelection}`
+    let winner = ``
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
+    if (playerSelection === `rock` && computerSelection === `scissors` || playerSelection === `scissors` && computerSelection === `paper` || playerSelection === `paper` && computerSelection === `rock`){
+        result = `You Win! You chose ${playerSelection} and ${playerSelection} beats ${computerSelection}`;
+        winner = `Player`
     }
-    else if (computerSelection === `rock` && playerSelection === `scissor` || computerSelection === `scissor` && playerSelection === `paper` || computerSelection === `paper` && playerSelection === `rock`){
-        result = `You Lose! ${computerSelection} beats ${playerSelection}`
+    else if (computerSelection === `rock` && playerSelection === `scissors` || computerSelection === `scissors` && playerSelection === `paper` || computerSelection === `paper` && playerSelection === `rock`){
+        result = `You Lose! You chose ${playerSelection} and ${computerSelection} beats ${playerSelection}`;
+        winner = `Computer`
     }
     else if (playerSelection === computerSelection){
-        result = `It's a tie!`
+        result = `It's a tie! You both chose ${playerSelection}`;
+        winner = `Nobody wins this round.`
     }
 
-    return result;
+    return [result, winner]
 }
 
-//Storing the play choices in variables that will be used as parameters for the playRound function
-let playerSelection = playerChoice;
-let computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
+//Implement function game() to play 5 rounds
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    let round;
+    let gameResult;
+    for (let i=0; i<5; i++){
+        round = playRound()
+        console.log(`Round ${i+1}: ${round[0]}`);
+        switch (`${round[1]}`) {
+            case `Player`:
+                playerScore ++;
+                break;
+            case `Computer`:
+                computerScore ++;
+                break;
+        }
+    }
+    if (playerScore > computerScore){
+        gameResult = `Yay! You are the winner!
+        Player Score: ${playerScore} Computer Score: ${computerScore}`;
+    }
+    else if (playerScore < computerScore){
+        gameResult = `Oh no, it was so close! You lose. Better luck in the next time.
+        Player Score: ${playerScore} Computer Score: ${computerScore}`;
+    }
+    else if (playerScore === computerScore){
+        gameResult = `What are the odds? It's a tie!
+        You both scored ${playerScore}`;
+    }
+    
+    return gameResult
+}
+console.log(game())
